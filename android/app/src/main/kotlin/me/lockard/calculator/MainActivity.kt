244085cd4f680548ef01dda717b86212ef931890
Jerry Lockard <quantum.ai.explorer@gmail.com>
2024-12-30 04:24:41 -0500

    calculator app

---
+++ android/app/src/main/kotlin/me/lockard/calculator/MainActivity.kt
@@ -0,0 +1,5 @@
+package me.lockard.calculator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/screen/main_screen.dart
@@ -0,0 +1,57 @@
+import 'package:calculator/controller/calculate_controller.dart';
+import 'package:calculator/controller/theme_controller.dart';
+import 'package:calculator/utils/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'sections/input_section.dart';
+import 'sections/output_section.dart';
+
+class MainScreen extends StatelessWidget {
+  MainScreen({super.key});
+
+  final List<String> buttons = [
+    "C",
+    "DEL",
+    "%",
+    "/",
+    "9",
+    "8",
+    "7",
+    "x",
+    "6",
+    "5",
+    "4",
+    "-",
+    "3",
+    "2",
+    "1",
+    "+",
+    "0",
+    ".",
+    "ANS",
+    "=",
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    var controller = Get.find<CalculateController>();
+    var themeController = Get.find<ThemeController>();
+
+    return GetBuilder<ThemeController>(builder: (context) {
+      return Scaffold(
+        backgroundColor: themeController.isDark
+            ? DarkColors.scaffoldBgColor
+            : LightColors.scaffoldBgColor,
+        body: Column(
+          children: [
+            GetBuilder<CalculateController>(builder: (context) {
+              return OutPutSection(themeController, controller);
+            }),
+            InPutSection(themeController, controller),
+          ],
+        ),
+      );
+    });
+  }
+}
